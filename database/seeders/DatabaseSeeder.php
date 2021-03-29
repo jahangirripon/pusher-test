20c9a434f9c8b3b2f3fdf2bfed10f608a48d96f4
jahangirripon <jripon1991@gmail.com>
2021-03-29 08:46:18 +0600

    Database seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Question;
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Like;
+use App\Models\Reply;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Category::factory(5)->create();
+        \App\Models\Question::factory(10)->create();
+        \App\Models\Reply::factory(50)->create();
+        \App\Models\Like::factory(500)->create();
+        // \App\Models\Reply::factory(10)->create()->each(function ($reply) {
+        //     return $reply->like()->save(\App\Models\Like::make());
+        // });
     }
 }
