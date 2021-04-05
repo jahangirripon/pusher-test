3e16d381d745c08d7ce558cbdfb993635028a7ee
jahangirripon <jripon1991@gmail.com>
2021-04-05 19:26:42 +0600

    Added JWT

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,20 @@ Route::resource('/question/{question}/reply', ReplyController::class);
 Route::post('/like/{reply}', [LikeController::class, 'likeIt']);
 Route::delete('/like/{reply}', [UserController::class, 'inlikeIt']);
 
+Route::group([
 
+    'middleware' => 'api',
+    'namespace' => 'App\Http\Controllers',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
